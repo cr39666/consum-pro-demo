4a14537272e8013f1ebeacbb853b36cb2b3e1238
v_hoyuechen <v_hoyuechen@tencent.com>
2024-08-20 17:30:44 +0800

    feat:添加环境变量，配置base

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/consum-pro-demo/' : '/',
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  build: {
-    outDir: "build"
-  }
-})
+export default defineConfig(({ command, mode }) => {
+
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    base: env.VITE_BASE_URL,
+    plugins: [
+      vue()
+    ],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+    build: {
+      outDir: "dist",
+    },
+  };
+});
